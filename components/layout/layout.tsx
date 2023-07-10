06e7f3a137bed1f06090a3b12ab6b99db5596ab0
Alex Tinsley <alex.j.tinsley@gmail.com>
2023-07-10 10:16:24 -0400

    Adding signup poppup from mailerlite

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -70,6 +70,14 @@ export const Layout = ({ rawData = {}, data = layoutData, children }) => {
             />
           </>
         )}
+        <script dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+            .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+            n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+            (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+            ml('account', '496565');`,
+          }}/>
       </Head>
       <Theme data={data?.theme}>
         <div
